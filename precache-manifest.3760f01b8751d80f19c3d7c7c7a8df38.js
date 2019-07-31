2f9617c533b77cd0f2adb815dbb17e0229f8d421
DaniilMarch <asmandian@gmail.com>
2019-07-31 11:39:18 +0300

    Updates

---
+++ precache-manifest.3760f01b8751d80f19c3d7c7c7a8df38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebad1b4254d9cb3947db8b796f78ddb",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "46d9a92356ea391a9a53",
+    "url": "/test-task/static/css/main.dbedc3e8.chunk.css"
+  },
+  {
+    "revision": "7dd743fb934a442b7298",
+    "url": "/test-task/static/js/2.8f7134b6.chunk.js"
+  },
+  {
+    "revision": "46d9a92356ea391a9a53",
+    "url": "/test-task/static/js/main.dfdbf488.chunk.js"
+  },
+  {
+    "revision": "b37567586fa682960a12",
+    "url": "/test-task/static/js/runtime~main.e21200f3.js"
+  }
+]);
